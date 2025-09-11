a0dc09ad586699e234126bc7ca41afc129114853
2emiln <2emiln@gmail.com>
2025-09-11 12:23:30 +0200

    added test_parse_number_basic

---
+++ tests/test_parse_number.py
@@ -0,0 +1,6 @@
+from src.moneygoal.io.avanza_csv import parse_number
+
+def test_parse_number_basic():
+    assert parse_number("1,23") == 1.23
+    assert parse_number("123") == 123.0
+    assert parse_number("-363 546,05") == -363546.05
